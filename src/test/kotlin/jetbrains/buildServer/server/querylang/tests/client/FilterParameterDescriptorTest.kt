0795fd4cb95ce4766c86f31f80e49b8e4a6e5f59
Ilya Voronin <voroninilya115@gmail.com>
2020-08-05 20:03:07 +0300

    add tests for step, feature and trigger filter

---
+++ src/test/kotlin/jetbrains/buildServer/server/querylang/tests/client/FilterParameterDescriptorTest.kt
@@ -0,0 +1,96 @@
+package jetbrains.buildServer.server.querylang.tests.client
+
+import jetbrains.buildServer.server.querylang.parser.QueryParser
+import jetbrains.buildServer.server.querylang.tests.BaseQueryLangTest
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+import kotlin.test.assertFailsWith
+
+class FilterParameterDescriptorTest : BaseQueryLangTest() {
+    override fun setUp() {
+        super.setUp()
+
+        TProject("BaseProject",
+            TTemplate("temp1",
+                TTrigger("vcsTrigger"),
+                TStep("step1"),
+                TFeature("feature1")
+            ).bind("t1"),
+
+            TBuildConf("test1",
+                TTrigger("vcsTrigger"),
+                TStep("step1"),
+                TFeature("feature1")
+            ).bind("b1"),
+
+            TBuildConf("test2",
+                TTrigger("schTrigger"),
+                TStep("step2"),
+                TFeature("feature2"),
+                TTempDependency("t1")
+            ).bind("b2"),
+
+            TBuildConf("test3",
+                TTrigger("trigger", Pair("path", "abc")),
+                TStep("step", Pair("abc", "def")),
+                TFeature("feature", Pair("asd", "fgh"))
+            ).bind("b3")
+        ).create()
+    }
+
+    @DataProvider(name = "data")
+    fun dataProvider() = TestDataProvider()
+        .addBCCase(
+            "find buildConfiguration with trigger type vcsTrigger",
+            "b1"
+        )
+        .addBCCase(
+            "find buildConfiguration with step type step1",
+            "b1"
+        )
+        .addBCCase(
+            "find buildConfiguration with feature type feature1",
+            "b1"
+        )
+        .addBCCase(
+            "find buildConfiguration with trigger[all] type vcsTrigger",
+            "b1", "b2"
+        )
+        .addBCCase(
+            "find buildConfiguration with step[all] type step1",
+            "b1", "b2"
+        )
+        .addBCCase(
+            "find buildConfiguration with feature[all] type feature1",
+            "b1", "b2"
+        )
+        .addBCCase(
+            "find buildConfiguration with trigger param path=abc and step param abc=def and feature param asd=fgh",
+            "b3"
+        )
+        .addTempCase(
+            "find template with trigger type vcsTrigger and step type step1 and feature type feature1",
+            "t1"
+        )
+        .end()
+
+    @DataProvider(name = "failed")
+    fun failedData() = TestFailedDataProvdier()
+        .addParseCase("find buildConfiguration with trigger[all vcsTrigger")
+        .addParseCase("find project with trigger type vcsTrigger")
+        .addParseCase("find buildConfiguration with trigger par abc=bcd")
+        .end()
+
+
+    @Test(dataProvider = "data")
+    fun parametrizedTest(query: String, expected: List<String>) {
+        val actual = getIds(query)
+
+        assertEquals(expected.sorted(), actual)
+    }
+
+    @Test(dataProvider = "failed")
+    fun parametrizedFailedParsingTest(query: String, exc: Class<out Exception>) {
+        assertFailsWith(exc.kotlin) { QueryParser.parse(query)}
+    }
+}
